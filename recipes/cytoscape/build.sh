e8176279f5881650ba6aeb9eea32e8bd2867d79c
Ista Zahn <istazahn@gmail.com>
2021-09-24 09:01:42 -0400

    fix cytoscape build

--- recipes/cytoscape/build.sh
+++ recipes/cytoscape/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
-envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}' < ${RECIPE_DIR}/response.varfile.template > response.varfile
-sh Cytoscape*.sh -q -varfile response.varfile
-envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}' < ${RECIPE_DIR}/cytoscape.sh.template > $outdir/cytoscape.sh
+mkdir -p "${PREFIX}/opt/cytoscape"
+cp -r ./* "${PREFIX}/opt/cytoscape/"
+echo '#!/bin/sh' > ${PREFIX}/bin/cytoscape
+echo '' >> ${PREFIX}/bin/cytoscape
+echo 'exec $(dirname $0)/../opt/cytoscape/cytoscape.sh $@' >> ${PREFIX}/bin/cytoscape
+chmod +x ${PREFIX}/bin/cytoscape
 
-ln -s $outdir/Cytoscape ${PREFIX}/bin
-ln -s $outdir/cytoscape.sh ${PREFIX}/bin
-ln -s $outdir/gen_vmoptions.sh ${PREFIX}/bin
